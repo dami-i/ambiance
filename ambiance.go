f58794fdb26b23aeb4631be474242f844c8d567e
dami-izolan-axon <dami.izolan@axontechnology.com.br>
2023-03-24 17:55:44 -0300

    main package quite done

--- ambiance.go
+++ ambiance.go
@@ -3,38 +3,128 @@ package ambiance
 import (
 	"errors"
 	"os"
-	"strings"
+	"path/filepath"
 	"runtime"
+	"strings"
 )
 
+var names = map[string]string{
+	"main":    ".env",
+	"sample":  ".env.sample",
+	"example": ".env.example",
+}
+
+type envObject map[string]string
+
 // Prepares the environment variables according to a .env file.
-func Prepare() error {
-	return errors.New("")
+func Config(dirPath string) {
+
+	basePath, err := os.Getwd()
+	if err != nil {
+		panic("error getting cwd")
+	}
+	envDir := filepath.Join(basePath, dirPath)
+
+	if exist := filesExist(envDir); !exist {
+		panic("unable to find '.env' and/or '.env.sample'/'.env.example' file(s)")
+	}
+
+	sampleFilename, err := sampleOrExample(envDir)
+	if err != nil {
+		panic(err)
+	}
+	mainContents, err := readTextFile(filepath.Join(envDir, names["main"]))
+	if err != nil {
+		panic(err)
+	}
+	sampleContents, err := readTextFile(filepath.Join(envDir, names[sampleFilename]))
+	if err != nil {
+		panic(err)
+	}
+	mainMap, err := mapVars(mainContents)
+	if err != nil {
+		panic(err)
+	}
+	sampleMap, err := mapVars(sampleContents)
+	if err != nil {
+		panic(err)
+	}
+
+	if !keysMatch(mainMap, sampleMap) {
+		panic("environment variables from main file don't match with sample file")
+	}
+
+	err = setEnv(mainMap)
+	if err != nil {
+		panic("error setting environment variables")
+	}
+
 }
 
 // Checks if all environment variables are properly set, according to the sample file.
-func precheck() error {
+//
+// dirPath is the relative path where sample file is located.
+func Check(dirPath string) error {
 	return errors.New("")
 }
 
-func readFile(filePath string) (string, error) {
-	return "", nil
+func filesExist(basePath string) bool {
+	env := filepath.Join(basePath, names["main"])
+	sample := filepath.Join(basePath, names["sample"])
+	example := filepath.Join(basePath, names["example"])
+
+	if _, err := os.Stat(env); errors.Is(err, os.ErrNotExist) {
+		return false
+	} else {
+		_, err1 := os.Stat(sample)
+		_, err2 := os.Stat(example)
+		if errors.Is(err1, os.ErrNotExist) && errors.Is(err2, os.ErrNotExist) {
+			return false
+		}
+	}
+	return true
 }
 
-func mapEnvVars(fileContents string) (map[string]string, error) {
-	envMap := make(map[string]string)
-	
-	file, err := os.Open(fileContents)
-	if err != nil {
-		return nil, err
-	}
+func sampleOrExample(basePath string) (string, error) {
+	sample := filepath.Join(basePath, names["sample"])
+	example := filepath.Join(basePath, names["example"])
 
+	if _, err := os.Stat(sample); err == nil {
+		return "sample", nil
+	} else if _, err := os.Stat(example); err == nil {
+		return "example", nil
+	} else {
+		return "", errors.New("sample file not found")
+	}
+}
+
+func keysMatch(mainMap, sampleMap envObject) bool {
+	for key := range mainMap {
+		if _, ok := sampleMap[key]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+// Returns a string with the text file contents
+func readTextFile(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
 	contents, err := os.ReadFile(file.Name())
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	lines := strings.Split(string(contents), eol())
+	return string(contents), file.Close()
+}
+
+func mapVars(fileContents string) (envObject, error) {
+	varMap := make(map[string]string)
+
+	lines := strings.Split(string(fileContents), eol())
 	for _, line := range lines {
 		line = strings.TrimSpace(line)
 		if string(line[0]) == "#" || line == "" {
@@ -48,10 +138,22 @@ func mapEnvVars(fileContents string) (map[string]string, error) {
 
 		key := strings.TrimSpace(parts[0])
 		value := strings.TrimSpace(parts[1])
-		envMap[key] = value
+		varMap[key] = value
 	}
 
-	return envMap, file.Close()
+	return varMap, nil
+}
+
+func setEnv(envMap envObject) error {
+	var err error
+	for key, value := range envMap {
+		err = os.Setenv(key, value)
+	}
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
 }
 
 func eol() string {
