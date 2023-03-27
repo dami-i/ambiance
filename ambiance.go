6ba4e108f94951f8318a51980803c36de6a3556f
dami-izolan-axon <dami.izolan@axontechnology.com.br>
2023-03-27 18:59:45 -0300

    refactor

--- ambiance.go
+++ ambiance.go
@@ -8,40 +8,91 @@ import (
 	"strings"
 )
 
-var names = map[string]string{
-	"main":    ".env",
-	"sample":  ".env.sample",
-	"example": ".env.example",
-}
+var (
+	filenames = map[string]string{
+		"main":   ".env",
+		"sample": ".env.sample",
+	}
+	nameOption = ".env.example"
+	cwd, _     = os.Getwd()
+	basePath   = filepath.Join(cwd) // initial value
+	paths      = map[string]string{
+		"main":   filepath.Join(cwd, filenames["main"]),   // initial value
+		"sample": filepath.Join(cwd, filenames["sample"]), // initial value
+	}
+)
 
 type envObject map[string]string
 
 // Prepares the environment variables according to a .env file.
+//
+// dirPath is the path to where the .env and sample files are located relative to the project's root.
 func Config(dirPath string) {
-
-	basePath, err := os.Getwd()
-	if err != nil {
-		panic("error getting cwd")
-	}
-	envDir := filepath.Join(basePath, dirPath)
-
-	if exist := filesExist(envDir); !exist {
-		panic("unable to find '.env' and/or '.env.sample'/'.env.example' file(s)")
+	basePath = filepath.Join(cwd, dirPath)
+	determineSampleFilename(basePath)
+	updatePaths(basePath)
+	if exist := filesExist(paths["main"], paths["sample"]); !exist {
+		panic("unable to find '.env' and/or sample file(s)")
 	}
 
-	sampleFilename, err := sampleOrExample(envDir)
-	if err != nil {
-		panic(err)
+	mainMap, sampleMap := parseEnvFiles(paths["main"], paths["sample"])
+	if !keysMatch(mainMap, sampleMap) {
+		panic("environment variables from main file don't match with the ones from sample file")
 	}
-	mainContents, err := readTextFile(filepath.Join(envDir, names["main"]))
+
+	err := setEnv(mainMap)
 	if err != nil {
-		panic(err)
+		panic("error setting environment variables")
 	}
-	sampleContents, err := readTextFile(filepath.Join(envDir, names[sampleFilename]))
+}
+
+// Checks if all environment variables are properly set, according to the sample file.
+//
+// dirPath is the relative path where sample file is located.
+func Check(dirPath string) error {
+	// @TODO
+	return errors.New("")
+}
+
+func determineSampleFilename(basePath string) {
+	sample := filepath.Join(basePath, filenames["sample"])
+	example := filepath.Join(basePath, nameOption)
+
+	if _, err := os.Stat(sample); err == nil {
+		// do nothing, as sample filename is already correct
+	} else if _, err := os.Stat(example); err == nil {
+		filenames["sample"] = nameOption
+	} else {
+		panic(".env.sample or .env.example is missing")
+	}
+}
+
+func updatePaths(dirPath string) {
+	paths["main"] = filepath.Join(dirPath, filenames["main"])
+	paths["sample"] = filepath.Join(dirPath, filenames["sample"])
+}
+
+func filesExist(mainPath, samplePath string) bool {
+	if _, err := os.Stat(mainPath); errors.Is(err, os.ErrNotExist) {
+		return false
+	} else if _, err1 := os.Stat(samplePath); errors.Is(err1, os.ErrNotExist) {
+		return false
+	}
+	return true
+}
+
+func parseEnvFiles(mainPath, samplePath string) (envObject, envObject) {
+
+	mainContents, err := readTextFile(mainPath)
 	if err != nil {
 		panic(err)
 	}
 	mainMap, err := mapVars(mainContents)
+	if err != nil {
+		panic(err)
+	}
+
+	sampleContents, err := readTextFile(samplePath)
 	if err != nil {
 		panic(err)
 	}
@@ -50,52 +101,8 @@ func Config(dirPath string) {
 		panic(err)
 	}
 
-	if !keysMatch(mainMap, sampleMap) {
-		panic("environment variables from main file don't match with sample file")
-	}
+	return mainMap, sampleMap
 
-	err = setEnv(mainMap)
-	if err != nil {
-		panic("error setting environment variables")
-	}
-
-}
-
-// Checks if all environment variables are properly set, according to the sample file.
-//
-// dirPath is the relative path where sample file is located.
-func Check(dirPath string) error {
-	return errors.New("")
-}
-
-func filesExist(basePath string) bool {
-	env := filepath.Join(basePath, names["main"])
-	sample := filepath.Join(basePath, names["sample"])
-	example := filepath.Join(basePath, names["example"])
-
-	if _, err := os.Stat(env); errors.Is(err, os.ErrNotExist) {
-		return false
-	} else {
-		_, err1 := os.Stat(sample)
-		_, err2 := os.Stat(example)
-		if errors.Is(err1, os.ErrNotExist) && errors.Is(err2, os.ErrNotExist) {
-			return false
-		}
-	}
-	return true
-}
-
-func sampleOrExample(basePath string) (string, error) {
-	sample := filepath.Join(basePath, names["sample"])
-	example := filepath.Join(basePath, names["example"])
-
-	if _, err := os.Stat(sample); err == nil {
-		return "sample", nil
-	} else if _, err := os.Stat(example); err == nil {
-		return "example", nil
-	} else {
-		return "", errors.New("sample file not found")
-	}
 }
 
 func keysMatch(mainMap, sampleMap envObject) bool {
@@ -133,7 +140,7 @@ func mapVars(fileContents string) (envObject, error) {
 
 		parts := strings.Split(line, "=")
 		if len(parts) != 2 {
-			return nil, errors.New("invalid env file contents")
+			return nil, errors.New("invalid file contents")
 		}
 
 		key := strings.TrimSpace(parts[0])
